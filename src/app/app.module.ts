8a6b053a67001fef81730dabf97d144f536abcd5
depanker <depanker@gmail.com>
2019-11-05 05:30:43 +0530

    Added login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,27 @@ import { UserProfileComponent } from './users/user-profile/user-profile.componen
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+// import {FirebaseUIModule} from 'firebaseui-angular';
+// import * as firebase from 'firebase/app';
+// import * as firebaseui from 'firebaseui';
+// currently there is a bug while building the app with --prod
+// - https://github.com/RaphaelJenni/FirebaseUI-Angular/issues/76
+// the plugin exposes the two libraries as well. You can use those:
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import {FirebaseUIModule, firebase, firebaseui} from 'firebaseui-angular';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,6 +56,9 @@ import { HttpClientModule } from '@angular/common/http';
     NbCardModule,
     NbAlertModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
